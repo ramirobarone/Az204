e0f47f73f5786c9518f480980b3f1850c5ca8274
rbarone <rbarone@PXSNTAR-16>
2023-03-19 23:52:16 -0300

    primer commit

---
+++ Az204Api/Controllers/AzController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Az204Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AzController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult BadRequest()
+        {
+            return NotFound("Error no encontrado");
+        }        
+    }
+}
