e86a1948a4f3ffd72ca51acbc2e349c248fe50ee
rbarone <rbarone@PXSNTAR-16>
2023-03-20 00:26:10 -0300

    segundo controller

---
+++ Az204Api/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Az204Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult MostrarSaludo(string nombre)
+        {
+            return Ok($"Hola {nombre} ");
+        }
+    }
+}
